9a1013ff102cac0ae7caa0a1560640bfef98bce4
Jashanpreetsingh <jashanpreetsingh49@dcmail.ca>
2025-10-29 23:30:54 -0400

    Created and customized navigation menu

--- wp-content/themes/studentSucess-child/functions.php
+++ wp-content/themes/studentSucess-child/functions.php
@@ -3,3 +3,8 @@
  * Child theme functions for Student Success Hub.
  * Add custom PHP here if needed.
  */
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+?>
